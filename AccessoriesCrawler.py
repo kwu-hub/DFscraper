c7638f06a44cc714a72a7b6e5fa2336ac9fee096
kevinwu1106 <kevinwu1106@gmail.com>
2018-12-22 20:07:17 +0800

    added all the bonuses

--- AccessoriesCrawler.py
+++ AccessoriesCrawler.py
@@ -34,66 +34,74 @@ def parse_item(html, data):
         # Need to use parent.parent to get the entire message (including the hyperlink)
         msg = msg.parent.parent
         if is_item(msg):
-            name = get_name(msg)
             equip = get_equip(msg)
-            item_type = get_type(msg)
+            name = get_name(msg)
             level = get_level(msg)
-            bonuses = get_bonus(msg)
-            element = get_element(msg)
             hyperlink = get_link(msg)
+            element = get_element(msg)
+            item_type = get_type(msg)
+            bonuses = get_bonus(msg)
+            bonus_dict = defaultdict(int)
+            if bonuses != 'None':
+                for bonus in bonuses.split(', '):
+                    if "+" in bonus :
+                        nb = bonus.split("+")
+                        bonus_dict[nb[0].split(" ")[0]] = int(nb[1])
+                    else:
+                        nb = bonus.split("-")
+                        bonus_dict[nb[0].split(" ")[0]] = int(nb[1])*-1
+
             print name + "( "+level+" ):" + bonuses + ":" + equip + ":" + item_type + ":" + element
-            print hyperlink
-            print "DC" + str(is_dc(msg))
-            print "rare" + str(is_rare(msg))
-            print "seasonal" + str(is_seasonal(msg))
-            print "da" + str(is_da(msg))
-            print "so" + str(is_so(msg))
-            print "dm" + str(is_dm(msg))
-            print "g" + str(is_g(msg))
             data[equip].append({
                 name + " (Level " + level + ")": {
+                    "equip": equip,
                     "name": name,
                     "level": level,
                     "link": hyperlink,
-                    "crit": 10,
-                    "bonus": 10,
-                    "str": 12,
-                    "dex": 12,
-                    "int": 12,
-                    "cha": 8,
-                    "luk": 8,
-                    "melee": 6,
-                    "pierce": 6,
-                    "magic": 6,
-                    "parry": 6,
-                    "block": 6,
-                    "dodge": 6,
-                    "end": 6,
-                    "wis": 6,
-                    "???": 0,
-                    "bacon": 0,
-                    "darkness": 0,
-                    "disease": 0,
-                    "energy": 0,
-                    "evil": 0,
-                    "fear": 0,
-                    "fire": 0,
-                    "good": 0,
-                    "ice": 0,
-                    "light": 0,
-                    "metal": 0,
-                    "nature": 10,
-                    "none": 0,
-                    "poison": 0,
-                    "silver": 0,
-                    "stone": 0,
-                    "water": 0,
-                    "wind": 0,
-                    "shrink": 0,
-                    "immobility": 0,
-                    "health": 0,
-                    "mana": 0,
-                    "all": 0,
+                    "element": element,
+                    "item_type": item_type,
+
+                    "crit": bonus_dict["Crit"],
+                    "bonus": bonus_dict["Bonus"],
+                    "str": bonus_dict["STR"],
+                    "dex": bonus_dict["DEX"],
+                    "int": bonus_dict["INT"],
+                    "cha": bonus_dict["CHA"],
+                    "luk": bonus_dict["LUK"],
+                    "melee": bonus_dict["Melee"],
+                    "pierce": bonus_dict["Pierce"],
+                    "magic": bonus_dict["Magic"],
+                    "parry": bonus_dict["Parry"],
+                    "block": bonus_dict["Block"],
+                    "dodge": bonus_dict["Dodge"],
+                    "end": bonus_dict["END"],
+                    "wis": bonus_dict["WIS"],
+
+                    "???": bonus_dict["???"],
+                    "bacon": bonus_dict["Bacon"],
+                    "darkness": bonus_dict["Darkness"],
+                    "disease": bonus_dict["Disease"],
+                    "energy": bonus_dict["Energy"],
+                    "evil": bonus_dict["Evil"],
+                    "fear": bonus_dict["Fear"],
+                    "fire": bonus_dict["Fire"],
+                    "good": bonus_dict["Good"],
+                    "ice": bonus_dict["Ice"],
+                    "light": bonus_dict["Light"],
+                    "metal": bonus_dict["Metal"],
+                    "nature": bonus_dict["Nature"],
+                    "none": bonus_dict["None"],
+                    "poison": bonus_dict["Poison"],
+                    "silver": bonus_dict["Silver"],
+                    "stone": bonus_dict["Stone"],
+                    "water": bonus_dict["Water"],
+                    "wind": bonus_dict["Wind"],
+                    "shrink": bonus_dict["Shrink"],
+                    "immobility": bonus_dict["Immobility"],
+                    "health": bonus_dict["Health"],
+                    "mana": bonus_dict["Mana"],
+                    "all": bonus_dict["All"],
+
                     "dc": is_dc(msg),
                     "da": is_da(msg),
                     "rare": is_rare(msg),
@@ -103,7 +111,6 @@ def parse_item(html, data):
                     "g": is_g(msg)
                 }
             })
-
 
 
 if __name__ == '__main__':
