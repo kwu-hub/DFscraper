b7b3273df469bf53842b0c115482e30a6e65ae78
kevinwu1106 <kevinwu1106@gmail.com>
2018-12-22 14:47:59 +0800

    Able to access each page and each accessory on that page

---
+++ AccessoriesCrawler.py
@@ -0,0 +1,63 @@
+try:
+    from BeautifulSoup import BeautifulSoup
+except ImportError:
+    from bs4 import BeautifulSoup
+import urllib2
+import json
+
+ROWS_TO_SKIP_ON_FIRST_PAGE = 3
+TOTAL_PAGES = 1
+ROWS_ON_PAGE = 5
+
+
+# Opens given page number and returns response
+def openPage(pageNumber):
+    response = urllib2.urlopen(
+        'http://forums2.battleon.com/f/tt.asp?forumid=118&p=' + str(pageNumber) + '&tmode=10&smode=1')
+    return response.read()
+
+
+def openItem(html):
+    link = html[html.find('tm.asp?m='):]
+    link = link[:link.find('"')]
+    sub_response = urllib2.urlopen("http://forums2.battleon.com/f/" + link)
+    return sub_response.read()
+
+
+def parseItem(html):
+    parsed_html = BeautifulSoup(html)
+    print parsed_html.body.find('td', attrs={'class': 'msg'}).text
+    # item type
+
+    # link
+    # name
+    # level
+    # bonuses
+    # element
+
+    # dc
+    # rare
+    # seasonal
+    # da
+    # so
+    # dm
+    # g
+
+
+if __name__ == '__main__':
+    for page in range(1, TOTAL_PAGES + 1):
+        tablePage = openPage(page)
+        for row in range(1, ROWS_ON_PAGE + 1):
+
+            # Find first instance of an accessory link in html range
+            # When found, we take a substring of the index+1 (removing '<') so exact string is not found again
+            rowIndex = tablePage.find('<a href="tm.asp?m=') + 1
+            tablePage = tablePage[rowIndex:]
+
+            # If we are scraping the first page, we skip the pinned topics at the top of the table
+            if page == 1 and row <= ROWS_TO_SKIP_ON_FIRST_PAGE:
+                continue
+
+            itemPage = openItem(tablePage)
+            parseItem(itemPage)
+    exit()
