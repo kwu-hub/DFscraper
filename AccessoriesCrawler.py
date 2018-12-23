3575c5dff22d267a4c6a63c2c2492985ba203176
kevinwu1106 <kevinwu1106@gmail.com>
2018-12-23 18:54:59 +0800

    Implemented optional DA, DC, Rare, Seasonal. Prints yeet when encounters a message with multiple entries with different bonuses

--- AccessoriesCrawler.py
+++ AccessoriesCrawler.py
@@ -1,13 +1,12 @@
 import urllib2
 import json
-from collections import defaultdict
 
 from BeautifulSoup import BeautifulSoup
 from Items import *
 
 ROWS_TO_SKIP_ON_FIRST_PAGE = 3
-STARTING_PAGE = 52
-TOTAL_PAGES = 52
+STARTING_PAGE = 73
+TOTAL_PAGES = 73
 
 # STARTING_PAGE = 1
 # TOTAL_PAGES = 81
@@ -50,126 +49,13 @@ def parse_item(html, data):
     for msg in msgs:
         # Need to use parent.parent to get the entire message (including the hyperlink)
         msg = msg.parent.parent
+
         if is_item(msg):
-            equip = get_equip(msg)
-            name = get_name(msg)
-            level = get_level(msg)
             hyperlink = get_link(msg)
-            element = get_element(msg)
-            item_type = get_type(msg)
-            bonuses = get_bonus(msg)
-
-            '''
-            Exceptions due to typos in the forums
-            Boxing Ring: Category and Equip Spot are in reverse order
-            Patrick's Emerald Green Hat: has 2 spaces after "Equip Spot:" instead of 1
-            '''
-            if name == "Boxing Ring":
-                equip = "finger"
-            if name == "Patrick's Emerald Green Hat":
-                equip = "head"
-
-            if name == "Grenwog Basket II":  # missing + in Pierce Def
-                bonuses = "Magic Def +1, Pierce Def +1, Melee Def +1, END +2, CHA +2, LUK +2, Bonus +1, Immobility +1, Stone +3, Wind +3 "
-            if name == "Golden Arbitrator VII":  # missing + in dodge
-                bonuses = "Magic Def +1, Pierce Def +1, Melee Def +1, END +2, CHA +2, LUK +2, Bonus +1, Immobility +1, Stone +3, Wind +3 "
-            if name == "Golden Arbitrator VIII":
-                bonuses = "Magic Def +1, Pierce Def +1, Melee Def +1, END +2, CHA +2, LUK +2, Bonus +1, Immobility +1, Stone +3, Wind +3 "
-            if name == "Golden Arbitrator IX":
-                bonuses = "Magic Def +1, Pierce Def +1, Melee Def +1, END +2, CHA +2, LUK +2, Bonus +1, Immobility +1, Stone +3, Wind +3 "
-            if name == "Grand Guardian Helm":  # missing + in pierce def
-                bonuses = "Magic Def +1, Pierce Def +1, Melee Def +1, END +2, CHA +2, LUK +2, Bonus +1, Immobility +1, Stone +3, Wind +3 "
-            if name == "Golden Battlespell Helm VII":  # missing + in dodge
-                bonuses = "Magic Def +1, Pierce Def +1, Melee Def +1, END +2, CHA +2, LUK +2, Bonus +1, Immobility +1, Stone +3, Wind +3 "
-            if name == "Golden Battlespell Helm VIII":
-                bonuses = "Magic Def +1, Pierce Def +1, Melee Def +1, END +2, CHA +2, LUK +2, Bonus +1, Immobility +1, Stone +3, Wind +3 "
-            if name == "Golden Battlespell Helm IX":
-                bonuses = "Magic Def +1, Pierce Def +1, Melee Def +1, END +2, CHA +2, LUK +2, Bonus +1, Immobility +1, Stone +3, Wind +3 "
-            if name == "High Commander Helm" and level == "28":  # missing WIS +
-                bonuses = "Magic Def +1, Pierce Def +1, Melee Def +1, END +2, CHA +2, LUK +2, Bonus +1, Immobility +1, Stone +3, Wind +3 "
-            if name == "Rokpol Ring":  # Missing comma before Bonus
-                bonuses = "Magic Def +1, Pierce Def +1, Melee Def +1, END +2, CHA +2, LUK +2, Bonus +1, Immobility +1, Stone +3, Wind +3 "
-            if name == "Silver Laurels V":  # Extra comma after immobility 64
-                bonuses = "Magic Def +1, Pierce Def +1, Melee Def +1, END +2, CHA +2, LUK +2, Bonus +1, Immobility +1, Stone +3, Wind +3 "
-            if name == "Silvered Bells Cape VII":  # . after water 64
-                bonuses = "Magic Def +1, Pierce Def +1, Melee Def +1, END +2, CHA +2, LUK +2, Bonus +1, Immobility +1, Stone +3, Wind +3 "
-            if name == "Silvered Bells Cape VIII":
-                bonuses = "Magic Def +1, Pierce Def +1, Melee Def +1, END +2, CHA +2, LUK +2, Bonus +1, Immobility +1, Stone +3, Wind +3 "
-            if name == "Wings of the Unchained" and level == "70":  # Parry probably isn't 34 and missing comma after
-                bonuses = "Magic Def +1, Pierce Def +1, Melee Def +1, END +2, CHA +2, LUK +2, Bonus +1, Immobility +1, Stone +3, Wind +3 "
-
-            print name + " (Level: " + level + "): " + bonuses + "|" + equip
-
-            bonus_dict = defaultdict(int)
-            # Creates dict of bonuses with key equal to the first word of the bonus (ex. Pierce Def->Pierce)
-            if bonuses != 'None':
-                for bonus in bonuses.split(', '):
-                    if "+" in bonus:
-                        nb = bonus.split("+")
-                        bonus_dict[nb[0].split(" ")[0]] = int(nb[1])
-                    else:
-                        nb = bonus.split("-")
-                        bonus_dict[nb[0].split(" ")[0]] = int(nb[1]) * -1
-
-            data[equip].append({
-                name + " (Level " + level + ")": {
-                    "equip": equip,
-                    "name": name,
-                    "level": level,
-                    "link": hyperlink,
-                    "element": element,
-                    "item_type": item_type,
-
-                    "crit": bonus_dict["crit"],
-                    "bonus": bonus_dict["bonus"],
-                    "str": bonus_dict["str"],
-                    "dex": bonus_dict["dex"],
-                    "int": bonus_dict["int"],
-                    "cha": bonus_dict["cha"],
-                    "luk": bonus_dict["luk"],
-                    "melee": bonus_dict["melee"],
-                    "pierce": bonus_dict["pierce"],
-                    "magic": bonus_dict["magic"],
-                    "parry": bonus_dict["parry"],
-                    "block": bonus_dict["block"],
-                    "dodge": bonus_dict["dodge"],
-                    "end": bonus_dict["end"],
-                    "wis": bonus_dict["wis"],
-
-                    "???": bonus_dict["???"],
-                    "bacon": bonus_dict["bacon"],
-                    "darkness": bonus_dict["darkness"],
-                    "disease": bonus_dict["disease"],
-                    "energy": bonus_dict["energy"],
-                    "evil": bonus_dict["evil"],
-                    "fear": bonus_dict["fear"],
-                    "fire": bonus_dict["fire"],
-                    "good": bonus_dict["good"],
-                    "ice": bonus_dict["ice"],
-                    "light": bonus_dict["light"],
-                    "metal": bonus_dict["metal"],
-                    "nature": bonus_dict["nature"],
-                    "none": bonus_dict["none"],
-                    "poison": bonus_dict["poison"],
-                    "silver": bonus_dict["silver"],
-                    "stone": bonus_dict["stone"],
-                    "water": bonus_dict["water"],
-                    "wind": bonus_dict["wind"],
-                    "shrink": bonus_dict["shrink"],
-                    "immobility": bonus_dict["immobility"],
-                    "health": bonus_dict["health"],
-                    "mana": bonus_dict["mana"],
-                    "all": bonus_dict["all"],
-
-                    "dc": is_dc(msg),
-                    "da": is_da(msg),
-                    "rare": is_rare(msg),
-                    "seasonal": is_seasonal(msg),
-                    "so": is_so(msg),
-                    "dm": is_dm(msg),
-                    "g": is_g(msg)
-                }
-            })
+            # Handles single message with multiple versions of item
+            if msg.getText().lower().count("bonuses") > 1:
+                print "yeet"
+            save_item(msg, data, hyperlink)
 
 
 if __name__ == '__main__':
--- Items.py
+++ Items.py
@@ -1,9 +1,127 @@
 # This file contains helper functions for parsing specific values from message
 import re
+from collections import defaultdict
 
 
-def save_item():
-    return
+def save_item(msg, data, hyperlink):
+    equip = get_equip(msg)
+    name = get_name(msg)
+    level = get_level(msg)
+    element = get_element(msg)
+    item_type = get_type(msg)
+    bonuses = get_bonus(msg)
+
+    '''
+    Exceptions due to typos in the forums
+    Boxing Ring: Category and Equip Spot are in reverse order
+    Patrick's Emerald Green Hat: has 2 spaces after "Equip Spot:" instead of 1
+    '''
+    if name == "Boxing Ring":
+        equip = "finger"
+    if name == "Patrick's Emerald Green Hat":
+        equip = "head"
+
+    if name == "Grenwog Basket II":  # missing + in Pierce Def
+        bonuses = "None +1"
+    if name == "Golden Arbitrator VII":  # missing + in dodge
+        bonuses = "None +1"
+    if name == "Golden Arbitrator VIII":
+        bonuses = "None +1"
+    if name == "Golden Arbitrator IX":
+        bonuses = "None +1"
+    if name == "Grand Guardian Helm":  # missing + in pierce def
+        bonuses = "None +1"
+    if name == "Golden Battlespell Helm VII":  # missing + in dodge
+        bonuses = "None +1"
+    if name == "Golden Battlespell Helm VIII":
+        bonuses = "None +1"
+    if name == "Golden Battlespell Helm IX":
+        bonuses = "None +1"
+    if name == "High Commander Helm" and level == "28":  # missing WIS +
+        bonuses = "None +1"
+    if name == "Rokpol Ring":  # Missing comma before Bonus
+        bonuses = "None +1"
+    if name == "Silver Laurels V":  # Extra comma after immobility 64
+        bonuses = "None +1"
+    if name == "Silvered Bells Cape VII":  # . after water 64
+        bonuses = "None +1"
+    if name == "Silvered Bells Cape VIII":
+        bonuses = "None +1"
+    if name == "Wings of the Unchained" and level == "70":  # Parry probably isn't 34 and missing comma after
+        bonuses = "None +1"
+
+    print name + " (Level: " + level + "): " + bonuses + "|" + equip
+
+    # Creates dict of bonuses with key equal to the first word of the bonus (ex. Pierce Def->Pierce)
+    bonus_dict = defaultdict(int)
+    if bonuses != 'None':
+        for bonus in bonuses.split(', '):
+            if "+" in bonus:
+                nb = bonus.split("+")
+                bonus_dict[nb[0].split(" ")[0]] = int(nb[1])
+            else:
+                nb = bonus.split("-")
+                bonus_dict[nb[0].split(" ")[0]] = int(nb[1]) * -1
+
+    data[equip].append({
+        name + " (Level " + level + ")": {
+            "equip": equip,
+            "name": name,
+            "level": level,
+            "link": hyperlink,
+            "element": element,
+            "item_type": item_type,
+
+            "crit": bonus_dict["crit"],
+            "bonus": bonus_dict["bonus"],
+            "str": bonus_dict["str"],
+            "dex": bonus_dict["dex"],
+            "int": bonus_dict["int"],
+            "cha": bonus_dict["cha"],
+            "luk": bonus_dict["luk"],
+            "melee": bonus_dict["melee"],
+            "pierce": bonus_dict["pierce"],
+            "magic": bonus_dict["magic"],
+            "parry": bonus_dict["parry"],
+            "block": bonus_dict["block"],
+            "dodge": bonus_dict["dodge"],
+            "end": bonus_dict["end"],
+            "wis": bonus_dict["wis"],
+
+            "???": bonus_dict["???"],
+            "bacon": bonus_dict["bacon"],
+            "darkness": bonus_dict["darkness"],
+            "disease": bonus_dict["disease"],
+            "energy": bonus_dict["energy"],
+            "evil": bonus_dict["evil"],
+            "fear": bonus_dict["fear"],
+            "fire": bonus_dict["fire"],
+            "good": bonus_dict["good"],
+            "ice": bonus_dict["ice"],
+            "light": bonus_dict["light"],
+            "metal": bonus_dict["metal"],
+            "nature": bonus_dict["nature"],
+            "none": bonus_dict["none"],
+            "poison": bonus_dict["poison"],
+            "silver": bonus_dict["silver"],
+            "stone": bonus_dict["stone"],
+            "water": bonus_dict["water"],
+            "wind": bonus_dict["wind"],
+            "shrink": bonus_dict["shrink"],
+            "immobility": bonus_dict["immobility"],
+            "health": bonus_dict["health"],
+            "mana": bonus_dict["mana"],
+            "all": bonus_dict["all"],
+
+            "dc": is_dc(msg),
+            "da": is_da(msg),
+            "rare": is_rare(msg),
+            "seasonal": is_seasonal(msg),
+            "so": is_so(msg),
+            "dm": is_dm(msg),
+            "g": is_g(msg)
+        }
+    })
 
 
 # Determines if the message received contains the attributes of an item
@@ -93,19 +211,41 @@ def get_link(html):
     return hyperlink
 
 
+# For DC, Rare, Seasonal, and DA tags we check if there are more than 1 entry (determined by multiple location: tags)
+# If there are multiple entries, we find the count of the given tag
+# If tag count is 0, then it is False
+# If tag count is same as entries, then it is True (each entry has it so it is not optional)
+# If tag count is neither, then it is optional (it only shows up on some of the entries)
+# Assumes Guardian, Special Offer, DM has no optional
 def is_dc(html):
+    images = len(html.findAll('img', src=re.compile("DC\.((jpg)|(png))")))
+    entry_count = html.getText().lower().count('location:')
+    if (images < entry_count) and entry_count > 1 and images != 0:
+        return "optional"
     return html.find('img', src=re.compile("DC\.((jpg)|(png))")) is not None
 
 
 def is_rare(html):
+    images = len(html.findAll('img', src=re.compile("Rare\.((jpg)|(png))")))
+    entry_count = html.getText().lower().count('location:')
+    if (images < entry_count) and entry_count > 1 and images != 0:
+        return "optional"
     return html.find('img', src=re.compile("Rare\.((jpg)|(png))")) is not None
 
 
 def is_seasonal(html):
+    images = len(html.findAll('img', src=re.compile("Seasonal\.((jpg)|(png))")))
+    entry_count = html.getText().lower().count('location:')
+    if (images < entry_count) and entry_count > 1 and images != 0:
+        return "optional"
     return html.find('img', src=re.compile("Seasonal\.((jpg)|(png))")) is not None
 
 
 def is_da(html):
+    images = len(html.findAll('img', src=re.compile("DA\.((jpg)|(png))")))
+    entry_count = html.getText().lower().count('location:')
+    if (images < entry_count) and entry_count > 1 and images != 0:
+        return "optional"
     return html.find('img', src=re.compile("DA\.((jpg)|(png))")) is not None
 
 
